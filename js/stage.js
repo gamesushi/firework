1b2d24905f2e8349edeffa38876a7df91e901576
hebeihang <hebeihang@gmail.com>
2025-12-30 20:39:15 +0900

    修复stage.js中的注释

--- js/stage.js
+++ js/stage.js
@@ -1,9 +1,20 @@
 /*
-Copyright © 2022 NianBroken. All rights reserved.
-Github：https://github.com/NianBroken/Firework_Simulator
-Gitee：https://gitee.com/nianbroken/Firework_Simulator
-本项目采用 Apache-2.0 许可证
-简而言之，你可以自由使用、修改和分享本项目的代码，但前提是在其衍生作品中必须保留原始许可证和版权信息，并且必须以相同的许可证发布所有修改过的代码。
+Copyright © 2022 Caleb Miller
+  CodePen: https://codepen.io/MillerTime/pen/XgpNwb
+
+Modifications Copyright © 2024 NianBroken (https://github.com/NianBroken/Firework_Simulator)
+Modifications Copyright © 2025 gamesushi
+GitHub: https://github.com/gamesushi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 
 const Ticker = (function TickerFactory(window) {
